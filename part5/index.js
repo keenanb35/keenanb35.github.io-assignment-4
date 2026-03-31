088ab084c2dd68bd2feccbd9802aa344540e28fb
keenanb35 <keenan.bombinomiller@dcmail.ca>
2026-03-31 16:55:33 -0400

    collect radio button selections

--- part5/index.js
+++ part5/index.js
@@ -1,4 +1,4 @@
-// index.js - Commit 2
+// index.js - Commit 3
 // Previous code
 const form = document.querySelector('form');
 
@@ -6,10 +6,16 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   console.log('Form submission prevented. Ready to process inputs.');
 
-  // Collect text input values
+  // Text inputs
   const name = document.querySelector('#name').value;
   const email = document.querySelector('#email').value;
   const phone = document.querySelector('#phone').value;
 
   console.log('Text inputs:', { name, email, phone });
+
+  // Radio button inputs
+  const porridge = document.querySelector('input[name="porridge"]:checked').value;
+  const beds = document.querySelector('input[name="beds"]:checked').value;
+
+  console.log('Radio selections:', { porridge, beds });
 });
