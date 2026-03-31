27704b2cb7f51800ab817e0a3e8de8402aff5086
keenanb35 <keenan.bombinomiller@dcmail.ca>
2026-03-31 16:39:38 -0400

    setup canvas, helpers, shape class

---
+++ part 4/main.js
@@ -0,0 +1,25 @@
+// main.js - Commit 1
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0,255)} ${random(0,255)} ${random(0,255)})`;
+}
+
+// Base Shape class
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
