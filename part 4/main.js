4b0bad2d47c3bb7aa33c51aa554ea4318241fe2e
keenanb35 <keenan.bombinomiller@dcmail.ca>
2026-03-31 16:40:33 -0400

    add ball class with draw(), update(), exists

--- part 4/main.js
+++ part 4/main.js
@@ -1,5 +1,6 @@
-// main.js - Commit 1
+// main.js - Commit 2
 
+// Canvas setup and helpers (from commit 1)
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -21,5 +22,31 @@ class Shape {
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+// Ball class
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) this.velX = -this.velX;
+    if (this.x - this.size <= 0) this.velX = -this.velX;
+    if (this.y + this.size >= height) this.velY = -this.velY;
+    if (this.y - this.size <= 0) this.velY = -this.velY;
+    this.x += this.velX;
+    this.y += this.velY;
   }
 }
